cdadff1e4e212f54d8598fb524cef6b5561c8f99
sdelamo <sergio.delamo@softamo.com>
2017-05-10 17:07:12 +0200

    remove comment

--- myapp/grails-app/conf/spring/resources.groovy
+++ myapp/grails-app/conf/spring/resources.groovy
@@ -1,6 +1,5 @@
 import demo.TestPersistenceEventListener
 
-// Place your Spring DSL code here
 beans = {
     testPersistenceEventListener(TestPersistenceEventListener, ref('hibernateDatastore'))
 }
